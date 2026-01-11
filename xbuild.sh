c0e2f574bbbf9e546eb0c7e23ed9a6785d716360
nico-bertoli <nicobertoli.1999@gmail.com>
2026-01-11 18:33:51 +0100

    google test integrated

--- xbuild.sh
+++ xbuild.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+echo ""
+echo "-------------------- Building"
+echo ""
+
 mkdir build
 cd build
-cmake -S .. -B .
-make
-cpack
+
+ #================================ google tests
+if [[ "$1" == "--test" ]]; then
+    
+    # set cmake BUILD_TESTING var to on
+    cmake -DBUILD_TESTING=ON .. 
+    make
+
+    echo ""
+    echo "-------------------- Running tests"
+    echo ""
+
+    ctest --output-on-failure   # Run tests with output on failure
+
+    echo ""
+    echo "All tests completed!"
+    
+#================================ normal build
+else 
+    cmake -S .. -B .
+    make
+    cpack
+fi
+
+
